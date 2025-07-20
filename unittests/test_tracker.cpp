711195a32fa3e97b429faf9cc1410fc9265fbeb7
VBStudi0s <vladybout@gmail.com>
2025-07-20 10:07:30 +0300

    Added basic tracker unittests

--- unittests/test_tracker.cpp
+++ unittests/test_tracker.cpp
@@ -1,6 +1,34 @@
 #include "gtest/gtest.h"
 
-TEST(sample_test_case, sample_test)
+#include "../src/core/Tracker/Tracker.hpp"
+
+constexpr int c_standing_time = 2*60;
+constexpr int c_day_end = 23*60*60 + 59*60 + 59;
+
+class TrackerTest : public testing::Test {
+ protected:
+  // TrackerTest() = default
+
+  // ~TrackerTest() override = default;
+
+  tracker::Tracker tr;
+};
+
+TEST_F(TrackerTest, empty)
 {
-    EXPECT_EQ(1, 1);
+    auto actual = tr.getDrivetime();
+    auto expected = std::pair<int, int>({c_day_end, 0});
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(TrackerTest, Basic)
+{
+    tr.addTimePoint({2, 1});
+    tr.addTimePoint({4, 2});
+    tr.addTimePoint({6, 0});
+    tr.addTimePoint({8, 1});
+
+    auto actual = tr.getDrivetime();
+    auto expected = std::pair<int, int>({c_day_end-2*3, 2*3});
+    EXPECT_EQ(actual, expected);
 }
